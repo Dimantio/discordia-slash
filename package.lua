f9220d74ac29799dd40a94101864a9ec45fe5756
Dimentio <dimentio@qcgames.org>
2022-08-11 10:05:32 -0400

    Create package.lua

---
+++ package.lua
@@ -0,0 +1,13 @@
+return {
+  name = "GitSparTV/discordia-slash",
+  version = "1.0.0",
+  description = "Discordia application commands extension ",
+  license = "MIT License",
+  author = { name = "GitSparTV", email = "" },
+  homepage = "https://github.com/Dimantio/discordia-slash",
+  files = {
+    "**.lua",
+    "!test*",
+    "!examples",
+  }
+}
